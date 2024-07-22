186b2e8c97043ee05e96ce59a6a5b226ccbac724
Atala2000 <atalakidi@gmail.com>
2024-07-22 14:46:29 +0300

    chore: Add Customer and Order models

--- tcfa/backend/models.py
+++ tcfa/backend/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Customer(models.Model):
+    code = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=15, null=False)
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    customer = models.ForeignKey(Customer, related_name='orders', on_delete=models.CASCADE)
+    item = models.CharField(max_length=255)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.item
