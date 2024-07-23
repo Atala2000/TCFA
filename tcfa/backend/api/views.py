19d21e4fd63c49b01dddb3d7e80306bb8df8abab
Atala2000 <atalakidi@gmail.com>
2024-07-23 14:24:28 +0300

    Refactor API URLs to use double quotes for consistency

--- tcfa/backend/api/views.py
+++ tcfa/backend/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 from ..models import Customer, Order
 from .serializers import CustomerSerializer, OrderSerializer
 from ..utils.africa_talks import send_sms
@@ -7,11 +8,13 @@ from ..utils.africa_talks import send_sms
 class CustomerListCreate(generics.ListCreateAPIView):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class OrderListCreate(generics.ListCreateAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
         order = serializer.save()
@@ -20,7 +23,7 @@ class OrderListCreate(generics.ListCreateAPIView):
     def send_sms_notification(self, order):
         customer = order.customer
         order_details = {
-            "sms_message": f"Order ({order.id}) for {customer.name}, your order for {order.item} has been received.",
+            "sms_message": f"Order ({order.order_code}) for {customer.name}, your order for {order.item} has been received.",
             "phone_no": f"{customer.phone}",
         }
         try:
@@ -28,3 +31,37 @@ class OrderListCreate(generics.ListCreateAPIView):
             print(response)
         except Exception as e:
             print(f"{e}")
+
+class OrderRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_update(self, serializer):
+        order = serializer.save()
+        self.send_sms_notification(order)
+
+    def send_sms_notification(self, order):
+        customer = order.customer
+        order_details = {
+            "sms_message": f"Order ({order.order_code}) for {customer.name}, your order for {order.item} has been updated.",
+            "phone_no": f"{customer.phone}",
+        }
+        try:
+            response = send_sms(order_details)
+            print(response)
+        except Exception as e:
+            print(f"{e}")
+
+    def perform_destroy(self, instance):
+        customer = instance.customer
+        order_details = {
+            "sms_message": f"Order ({instance.id}) for {customer.name}, your order for {instance.item} has been deleted.",
+            "phone_no": f"{customer.phone}",
+        }
+        try:
+            response = send_sms(order_details)
+            print(response)
+        except Exception as e:
+            print(f"{e}")
+        instance.delete()
