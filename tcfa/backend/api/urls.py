ada0038f689b50af94e17501b8304a6fed8190c4
Atala2000 <atalakidi@gmail.com>
2024-07-23 14:21:59 +0300

    Refactor API URLs to use double quotes for consistency

--- tcfa/backend/api/urls.py
+++ tcfa/backend/api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import CustomerListCreate, OrderListCreate
 
 urlpatterns = [
-    path('customers/', CustomerListCreate.as_view(), name='customer-list-create'),
-    path('orders/', OrderListCreate.as_view(), name='order-list-create'),
+    path("customers/", CustomerListCreate.as_view(), name="customer-list-create"),
+    path("orders/", OrderListCreate.as_view(), name="order-list-create"),
 ]
