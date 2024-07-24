267c90be23b0e6f1e4657936f543ee074505c3a0
Atala2000 <atalakidi@gmail.com>
2024-07-24 10:55:16 +0300

    chore: Update API URLs for user authentication and add Auth0 integration

--- tcfa/backend/api/urls.py
+++ tcfa/backend/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import CustomerListCreate, OrderListCreate
+from .views import CustomerListCreate, OrderListCreate, OrderRetrieveUpdateDestroy
 
 urlpatterns = [
-    path("customers/", CustomerListCreate.as_view(), name="customer-list-create"),
-    path("orders/", OrderListCreate.as_view(), name="order-list-create"),
+    path('customers/', CustomerListCreate.as_view(), name='customer-list-create'),
+    # path('customers/<uuid:pk>/', CustomerRetrieveUpdateDestroy.as_view(), name='customer-detail'),
+    path('orders/', OrderListCreate.as_view(), name='order-list-create'),
+    path('orders/<uuid:pk>/', OrderRetrieveUpdateDestroy.as_view(), name='order-detail'),
 ]
