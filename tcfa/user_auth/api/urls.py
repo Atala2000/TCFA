3a1709f43a09430b1d6bfe24a439f47e563295ae
Atala2000 <atalakidi@gmail.com>
2024-07-24 15:32:04 +0300

    chore: Remove unused get_user API endpoint

--- tcfa/user_auth/api/urls.py
+++ tcfa/user_auth/api/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import auth0_login, auth0_callback, logout, get_user
+from .views import auth0_login, auth0_callback, logout
 
 urlpatterns = [
     path('auth/login/', auth0_login, name='auth0_login'),
     path('auth/callback/', auth0_callback, name='auth0_callback'),
     path('auth/logout/', logout, name='logout'),
-    path('auth/user/', get_user, name='get_user'),
 ]
 
