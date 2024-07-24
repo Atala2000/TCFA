235b0ad0d539dbb9f2276bf5949e335f9887c03e
Atala2000 <atalakidi@gmail.com>
2024-07-24 15:18:23 +0300

    chore: Add index view with API endpoints

--- tcfa/backend/views.py
+++ tcfa/backend/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+def index(request):
+    return JsonResponse({
+        "message": "Welcome to an Order API service",
+        "endpoints": {
+            "/api/customers": "Get a list of customers",
+            "/api/orders": "Get a list of orders",
+            "/user/auth/login": "Authenticate and login",
+            "/user/auth/logout": "Logout and end session"
+        }
+    })
