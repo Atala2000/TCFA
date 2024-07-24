7f1fe8cc8e334090be78925bdcf3dc5c687a28c0
Atala2000 <atalakidi@gmail.com>
2024-07-24 15:18:31 +0300

    chore: Remove commented out URL for customer detail view

--- tcfa/backend/api/urls.py
+++ tcfa/backend/api/urls.py
@@ -3,7 +3,6 @@ from .views import CustomerListCreate, OrderListCreate, OrderRetrieveUpdateDestr
 
 urlpatterns = [
     path('customers/', CustomerListCreate.as_view(), name='customer-list-create'),
-    # path('customers/<uuid:pk>/', CustomerRetrieveUpdateDestroy.as_view(), name='customer-detail'),
     path('orders/', OrderListCreate.as_view(), name='order-list-create'),
     path('orders/<uuid:pk>/', OrderRetrieveUpdateDestroy.as_view(), name='order-detail'),
 ]
